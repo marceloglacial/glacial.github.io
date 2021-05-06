c4618924b1c2629fd3fe76a65c16e5d84359f161
Marcelo <glacial@gmail.com>
2021-05-05 21:55:36 -0400

    add google analytics

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,8 +1,26 @@
 import Head from 'next/head';
 
 const Header = (props) => {
+  const GA_MEASUREMENT_ID = `UA-12989788-1`;
   return (
     <Head>
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <script
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{
+          __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_MEASUREMENT_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+        }}
+      />
       <title>
         Marcelo Glacial - Front-End Developer, Web Developer and Web Designer
       </title>
